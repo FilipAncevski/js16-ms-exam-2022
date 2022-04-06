b3a10084cde1fa719ac6f785d06215068a52894a
FilipAncevski <filip.anchevski1@gmail.com>
2022-04-06 22:17:16 +0200

    GGWP

--- TEST/handlers/index.js
+++ TEST/handlers/index.js
@@ -10,6 +10,8 @@ const {
   computerUpdate,
   validate,
 } = require("../pkg/portable-computer/validate");
+const jwt = require("jsonwebtoken");
+const { get } = require("../pkg/config");
 
 const listAll = async (req, res) => {
   try {
@@ -26,7 +28,6 @@ const add = async (req, res) => {
     const pc = await create(req.body);
     return res.status(201).send(pc);
   } catch (error) {
-    console.error(error);
     return res.status(500).send("Internal Server Error");
   }
 };
@@ -34,7 +35,16 @@ const add = async (req, res) => {
 const listOne = async (req, res) => {
   try {
     const pc = await listOnePc(req.params.id);
-    console.log(pc);
+
+    // const payload = {
+    //   brand: pc.brand,
+    //   model: pc.model,
+    //   id: pc._id,
+    //   exp: new Date().getTime() / 1000 + 7 * 24 * 60 * 60,
+    // };
+
+    // const token = jwt.sign(payload, get("service").jwt_key);
+
     return res.status(200).send(pc);
   } catch (error) {
     console.log(error);
@@ -48,7 +58,6 @@ const update = async (req, res) => {
     const up = await updatePc(req.params.id, req.body);
     return res.status(204).send(" ");
   } catch (error) {
-    console.log(error);
     return res.status(500).send("Internal Server Error");
   }
 };
--- TEST/pkg/portable-computer/validate.js
+++ TEST/pkg/portable-computer/validate.js
@@ -1,21 +1,21 @@
 const { Validator } = require("node-input-validator");
 
 const computerCreate = {
-  brand: "require|string",
-  model: "require|string",
-  cpu_cores: "require|integer",
-  ram: "require|integer",
-  disk: "require|string",
-  price: "require|integer",
-  stores: "require|array",
-  num_ports: "require|object",
-  create: "require|date",
+  brand: "required|string",
+  model: "required|string",
+  cpu_cores: "required|between:1,16",
+  ram: "required|integer",
+  disk: "required|string",
+  price: "required|integer",
+  stores: "required|array",
+  num_ports: "required|object",
+  create: "required|date",
 };
 
 const computerUpdate = {
   brand: "string",
   model: "string",
-  cpu_cores: "integer|between:1,16",
+  cpu_cores: "integer",
   ram: "integer",
   disk: "string",
   price: "integer",
@@ -26,11 +26,12 @@ const computerUpdate = {
 
 const validate = async (data, schema) => {
   const v = new Validator(data, schema);
+  // let cpu = new Validator({ cpu_cores: "" }, { age: "required|between:1,16" });
   const c = await v.check();
   if (!c) {
     throw {
-      error: 400,
-      code: "Missing required fields",
+      code: 400,
+      message: "Missing required fields",
     };
   }
 };
